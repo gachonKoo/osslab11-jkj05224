3dd8a5c0f26e000a7605463347dde151dae6729b
jkj05224 <jkj05224@naver.com>
2025-11-16 04:42:02 +0900

    과제제출

---
+++ .github/workflows/tester.py
@@ -0,0 +1,11 @@
+import geo.utils as utils
+
+# calculate the length of hypotenuse
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print('c =', c)
+
+# calculate the area of a circle
+r = 10
+area = utils.circle(r)
+print('area =', area)
